b5ed933439a67384c50e7b5f5e0bd6999e308ba0
yaroslavmanko <9276307727@my.lerna.by>
2026-08-15 18:49:09 +0300

    измененный app.py

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-import smtplib
-from email.message import EmailMessage
-import re
-import html
 import os
-from dotenv import load_dotenv
+from flask_cors import CORS
+from flask import Flask, render_template
 
-load_dotenv()
 
-# Инициализируем приложение. Указываем папки для статики и шаблонов
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
 @app.route('/')
 def home():
-    # Эта функция берет index.html из папки templates и показывает его пользователю
     return render_template('index.html')
 
 
-
 if __name__ == '__main__':
-    # Оставляем так для локального теста. На сервере это запустится через gunicorn
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
